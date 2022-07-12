85b2ca74a9acbeedda30e36af7bf6322ee654076
Gustavo Rodriguez <gustavojrs01@gmail.com>
2022-07-12 18:25:22 -0400

    Curso

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM  from "react-dom/client";
+
+function App() {
+    return (
+        <div className="container">
+            <h1>Hola Mundo</h1>
+            <h2>Esto es un h2</h2>
+        </div>
+    );
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);  
